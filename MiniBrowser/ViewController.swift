82936c50ad6703d329db5c781b478b9ea22bc5d3
Vitas <arentafor@gmail.com>
2021-05-31 13:51:00 +0300

    Initial Commit

---
+++ MiniBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniBrowser
+//
+//  Created by Виталий Крюков on 31.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
