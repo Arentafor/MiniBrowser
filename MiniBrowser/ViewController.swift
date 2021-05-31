2c7ef8f19192f603bf6baea15622ab1048d765fc
Vitas <arentafor@gmail.com>
2021-05-31 16:31:09 +0300

    release 1.0

--- MiniBrowser/ViewController.swift
+++ MiniBrowser/ViewController.swift
@@ -6,14 +6,80 @@
 //
 
 import UIKit
+import WebKit
+import SVProgressHUD
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func backButtom(_ sender: Any) {
+        webView.goBack()
+    }
+    
+    @IBAction func nextButtom(_ sender: Any) {
+        webView.goForward()
+    }
+    
+    @IBOutlet weak var seachBarTextfield: UITextField!
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    @IBAction func closeKeyboard(_ sender: Any) {
+        seachBarTextfield.resignFirstResponder()
+    }
+    
+    @IBAction func finderButtom(_ sender: Any) {
+        if let text = seachBarTextfield.text {
+            if let search = URL(string: text) {
+                let request = URLRequest(url: search)
+                webView.load(request)
+                SVProgressHUD.show(withStatus: "Loading...")
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        webView.navigationDelegate = self
+        seachBarTextfield.delegate = self
     }
+}
 
+extension ViewController: UITextViewDelegate {
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+          if (text == "\n") {
+              textView.resignFirstResponder()
+          }
+          return true
+      }
+}
 
+extension ViewController: WKNavigationDelegate {
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        SVProgressHUD.dismiss()
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        SVProgressHUD.dismiss()
+    }
+    
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        SVProgressHUD.dismiss()
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let text = seachBarTextfield.text {
+            if let search = URL(string: text) {
+                let request = URLRequest(url: search)
+                webView.load(request)
+                SVProgressHUD.show(withStatus: "Loading...")
+            }
+        }
+        return true
+    }
+    
+    
 }
 
